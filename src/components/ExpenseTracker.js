2f3e239837d0288c96288764b662333ad14b5ff2
artikgupta <artig9262@gmail.com>
2020-07-23 16:29:41 +0530

    display transaction and amount and type  value of  the input feild

---
+++ src/components/ExpenseTracker.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+
+class ExpenseTracker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      transactionName: "",
+      amount: "",
+      IncomeAndAmountArray: [],
+      type: "income",
+      isTrue: false,
+    };
+  }
+
+  transactionNameHandler = (e) => {
+    this.setState({
+      transactionName: e.target.value,
+    });
+  };
+
+  amountHandler = (e) => {
+    this.setState({
+      amount: e.target.value,
+    });
+  };
+
+  typeChangeHandler = (e) => {
+    this.setState({
+      type: e.target.value,
+    });
+  };
+
+  addTransaction = () => {
+    if (this.state.transactionName === "" || this.state.amount === "") {
+      alert("Your input feild is empty,please enter a valid value");
+    } else {
+      const newIncAndAmt = [...this.state.IncomeAndAmountArray];
+      newIncAndAmt.push({
+        transactionName: this.state.transactionName,
+        amount: this.state.amount,
+        type: this.state.type,
+      });
+      this.setState({
+        isTrue: true,
+        transactionName: "",
+        amount: "",
+        IncomeAndAmountArray: newIncAndAmt,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="main-container">
+        <div className="container">
+          <div className="trans-value">
+            <label>
+              Transaction
+              <input
+                type="text"
+                value={this.state.transactionName}
+                onChange={this.transactionNameHandler}
+              />
+            </label>
+          </div>
+
+          <div className="trans-amt">
+            <label>
+              Amount
+              <input
+                type="number"
+                value={this.state.amount}
+                onChange={this.amountHandler}
+              />
+            </label>
+          </div>
+
+          <div className="typeName">
+            <label>
+              Transaction type
+              <select value={this.state.type} onChange={this.typeChangeHandler}>
+                <option value="expense">Expense</option>
+                <option value="income">Income</option>
+              </select>
+            </label>
+          </div>
+          <div className="btn">
+            <button onClick={this.addTransaction}>Add</button>
+          </div>
+        </div>
+        {this.state.isTrue ? (
+          <div className="flex">
+            <h4>Type</h4>
+            <h4>TransactionName</h4>
+            <h4>Amount</h4>
+          </div>
+        ) : (
+          ""
+        )}
+        {this.state.IncomeAndAmountArray.map((source) => {
+          return (
+            <div key={source.transactionName}>
+              <div className="transaction">
+                <p>{source.type}</p>
+                <p>{source.transactionName}</p>
+                <p>
+                  <i className="fas fa-rupee-sign"></i>
+                  {source.amount}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default ExpenseTracker;
